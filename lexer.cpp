4bf43f9ba64003862604f8d8cd5d43ab2a9631ba
Hanyuan Ye <yehanyuan1999@gmail.com>
2018-06-03 16:21:29 -0400

    added oprators to lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,7 +1,7 @@
 #include "lexer.h"
 
 Lexer::Lexer() {
-	typeRegex = std::regex("return|int|;|\\(|\\)|\\{|\\}|[_a-zA-Z][_a-zA-Z0-9]{0,30}*");
+	typeRegex = std::regex("return|int|;|\\(|\\)|\\{|\\}|[_a-zA-Z][_a-zA-Z0-9]{0,30}*|+|-|*|/");
 	numRegex = std::regex("[0-9]+");
 	std::vector<std::string> typeArr = {
 		"return",
@@ -10,7 +10,11 @@ Lexer::Lexer() {
 		"(",
 		")",
 		"{",
-		"}"
+		"}",
+		"+",
+		"-",
+		"*",
+		"/"
 	};
 	for (const auto& s : typeArr) {
 		typeDictionary.insert(s);
