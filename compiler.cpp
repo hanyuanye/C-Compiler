430ea5b8a6f366ddb25f018191b3a77cb4f0edf2
Hanyuan Ye <yehanyuan1999@gmail.com>
2018-06-05 13:03:18 -0400

    fixed a bug involving compiler output

--- compiler.cpp
+++ compiler.cpp
@@ -6,7 +6,7 @@
 #include "AsmGenerator.h"
 int main(int argc, char** argv)
 {
-	std::string code = "int main() {return 1==2;}"; //edit this string to edit source code being compiled
+	std::string code = "int main() {return 120<1*3*4*5;}"; //edit this string to edit source code being compiled
 	Lexer lexer;
 	lexer.lex(code);
 	Parser parser(lexer.getTokens());
@@ -15,8 +15,8 @@ int main(int argc, char** argv)
 //	}
 
 	parser.parse();
-	parser.print();
+//	parser.print();
 	AsmGenerator asmGen;
 	asmGen.generateAssembly(parser.ast);
-//	asmGen.emit();
+	asmGen.emit();
 }
