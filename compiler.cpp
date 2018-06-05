870a6abe2c10d2111392befa9bd78ad2e7d1f02e
Hanyuan Ye <yehanyuan1999@gmail.com>
2018-06-04 21:11:46 -0400

    updated names

--- compiler.cpp
+++ compiler.cpp
@@ -6,9 +6,9 @@
 #include "AsmGenerator.h"
 int main(int argc, char** argv)
 {
-	std::string s = "int main() {return (1+1)/2;}";
+	std::string code = "int main() {return (1+1)/2;}"; //edit this string to edit source code being compiled
 	Lexer lexer;
-	lexer.lex(s);
+	lexer.lex(code);
 	Parser parser(lexer.getTokens());
 //	for (auto const& t: lexer.getTokens()) {
 //		std::cout << t.first << " " << t.second.type << " " << t.second.value << std::endl;
