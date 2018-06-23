3b5bf85d32b429d9e9646a94886e46fb5454bc6a
Hanyuan Ye <yehanyuan1999@gmail.com>
2018-06-23 02:33:30 -0400

    function stuff

--- AstNode.cpp
+++ AstNode.cpp
@@ -2,27 +2,27 @@
 
 AstNode::AstNode() {}
 
-AstNode::AstNode(AstType _type) {
+AstNode::AstNode(std::string _type) {
 	type = _type;
 }
 
-AstNode::AstNode(AstType _type, std::string _value) {
+AstNode::AstNode(std::string _type, std::string _value) {
 	type = _type;
 	value = _value;
 }
 
-AstNode::AstNode(AstType _type, AstNode child) {
+AstNode::AstNode(std::string _type, AstNode child) {
 	type = _type;
 	children.emplace_back(new AstNode(child));
 }
 
-AstNode::AstNode(AstType _type, std::string _value, AstNode child) {
+AstNode::AstNode(std::string _type, std::string _value, AstNode child) {
 	type = _type;
 	value = _value;
 	children.emplace_back(new AstNode(child));
 }
 
-void AstNode::addType(AstType _type) {
+void AstNode::addType(std::string _type) {
 	type = _type;
 }
 
--- AstNode.h
+++ AstNode.h
@@ -37,16 +37,16 @@ enum AstType {
 class AstNode {
 	public:
 		AstNode();
-		AstNode(AstType _type);
-		AstNode(AstType _type, std::string _value);
-		AstNode(AstType _type, AstNode child);
-		AstNode(AstType _type, std::string _value, AstNode child);
-		void addType(AstType _type);
+		AstNode(std::string _type);
+		AstNode(std::string _type, std::string _value);
+		AstNode(std::string _type, AstNode child);
+		AstNode(std::string _type, std::string _value, AstNode child);
+		void addType(std::string _type);
 		void addValue(std::string _value);
 		void addNode(AstNode node);
 		void addNodeFront(AstNode node);
 		void clearNodes();
-		AstType type;
+		std::string type;
 		std::string value;
 		bool scopeFlag; //Scope flag is used only by function nodes to determine if a return statement was declared in the function body
 		std::deque< std::shared_ptr<AstNode> > children;
